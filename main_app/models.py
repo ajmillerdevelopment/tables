8c00d73d79fe37f25b88117b30a1b173e631604a
Alex Miller <ajm9594@gmail.com>
2021-02-06 17:07:38 -0800

    table create route

--- main_app/models.py
+++ main_app/models.py
@@ -14,17 +14,17 @@ CATS = (
 class Item(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=3, choices=CATS)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     def __str__(self):
         return self.name
 
 class Table(models.Model):
     name = models.CharField(max_length=100)
     number = models.IntegerField(default=0)
-    bill = models.DecimalField(max_digits=8, decimal_places=2)
-    grat15 = models.DecimalField(max_digits=6, decimal_places=2)
-    grat18 = models.DecimalField(max_digits=6, decimal_places=2)
-    grat20 = models.DecimalField(max_digits=6, decimal_places=2)
+    bill = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    grat15 = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    grat18 = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    grat20 = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     dtg = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f'Table {self.number}'
@@ -33,6 +33,6 @@ class Cover(models.Model):
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
     seat = models.IntegerField
     items = models.ManyToManyField(Item)
-    subtot = models.DecimalField(max_digits=8, decimal_places=2)
+    subtot = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     def __str__(self):
         return f'Table {self.table} Seat {self.seat}'
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect
 from .models import *
-
+from .forms import *
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -14,7 +15,14 @@ def table_detail(request, table_id):
     table = Table.objects.get(id=table_id)
     return render(request, 'table_detail.html', {'table': table})
 def table_new(request):
-    return render(request, 'table_new.html')
+    if request.method == 'POST':
+        form = TableForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    else:
+        form = TableForm()
+        return render(request, 'table_new.html',{ 'form': form})
 def table_close(request, table_id):
     table = Table.objects.get(id=table_id)
     return render(request, 'table_close.html', {'table': table})
