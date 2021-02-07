21ac22aa6ba440de708eda11d0e0b592d9eaf4ae
Alex Miller <ajm9594@gmail.com>
2021-02-06 17:12:45 -0800

    show tables

--- main_app/views.py
+++ main_app/views.py
@@ -5,15 +5,19 @@ from .forms import *
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
 def menu(request):
     items = Item.objects.all()
     return render(request, 'menu.html', {'items': items})
+
 def tables_index(request):
     tables = Table.objects.all()
     return render(request, 'tables.html', {'tables': tables})
+
 def table_detail(request, table_id):
     table = Table.objects.get(id=table_id)
     return render(request, 'table_detail.html', {'table': table})
+
 def table_new(request):
     if request.method == 'POST':
         form = TableForm(request.POST)
@@ -23,6 +27,7 @@ def table_new(request):
     else:
         form = TableForm()
         return render(request, 'table_new.html',{ 'form': form})
+
 def table_close(request, table_id):
     table = Table.objects.get(id=table_id)
     return render(request, 'table_close.html', {'table': table})
